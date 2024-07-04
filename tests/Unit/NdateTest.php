3d71ae748a35f48ae94470e5680e81468f93ff17
Kamal Pandey <kamal.pandey@peacenepal.com.np>
2024-07-04 11:19:16 +0545

    Merge pull request #1 from kmlpandey77/kmlpandey77-patch-1
    2081 month change

--- tests/Unit/NdateTest.php
+++ tests/Unit/NdateTest.php
@@ -20,3 +20,13 @@ test('Invalid Date', function () {
     expect(fn () => Ndate::to_bs($ad))
         ->toThrow(InvalidDateException::class);
 });
+
+
+test('Asar 2081 date issue', function () {
+    $ad = '2024-07-03';
+    $bs = '2081-03-19';
+
+    $date_to_bs = Ndate::to_bs($ad);
+
+    expect("{$date_to_bs}")->toBe($bs);
+});
